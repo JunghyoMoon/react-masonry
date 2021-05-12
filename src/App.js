148f8e26360d8a7a864e87f830f529b753cc2b04
JungHyoMoon <dsinhome70@gmail.com>
2021-05-12 09:52:18 +0900

    todo: photo item styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Main from "./routes/Main";
+import "./styles.css";
 
 function App() {
     return <Main />;
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -4,8 +4,8 @@ function Photo({ img, user, tags }) {
     return (
         <div className="photo">
             <img src={img} alt={tags} title={tags}></img>
-            <span>{user}</span>
-            <span>{tags}</span>
+            <span className="photo__user">{user}</span>
+            <span className="photo__tags">{tags}</span>
         </div>
     );
 }
